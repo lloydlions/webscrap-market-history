e000516c2d98f5d6f80b57da55bd06906842e36c
Lloyd Liongson <user@LloydLions.local>
2022-03-28 01:03:56 +0800

    creating steps for excel.js

--- index.ts
+++ index.ts
@@ -1,41 +1,61 @@
 import puppeteer from "puppeteer";
+import fs from "fs";
+import ExcelJS from "exceljs";
 
+//npx ts-node index.ts
 (async () => {
-  const browser = await puppeteer.launch({
-    // headless: false,
-    defaultViewport: {
-      width: 1800,
-      height: 1800,
-    },
-  });
+  const coins = [
+    "BTC-USD", //bitcoin
+  ];
 
-  const page = await browser.newPage();
+  let instance = 0;
 
-  await page.goto("https://finance.yahoo.com/quote/BTC-USD/history?p=BTC-USD");
-  let closePrice =
-    '//table[@data-test="historical-prices"]//tbody//tr[2]//td[5]//span';
+  let d = new Date();
+  const path = d.toLocaleDateString() + ".xslx";
 
-  console.log("90 Days Date: ", return90DaysDate());
-  const datas = await page.$$eval("table tbody tr", (tds) =>
-    tds.map((td) => {
-      return (td as HTMLElement).innerText;
-    })
-  );
+  const workbook = new ExcelJS.Workbook();
+  workbook.creator = "LRL";
+  workbook.lastModifiedBy = "LRL-bot";
+  workbook.modified = new Date();
 
-  console.log(datas);
-  for (let i = 0; i <= 90; i++) {
-    //   //Date
-    //   //Open
-    //   //High
-    //   //Low
-    //   //Close
-    //   //Adj Close*
-    //   //Volume
-    const data = datas[i].split("\t");
-    console.log("DAtA: ", data[4]);
+  //loop through all coins
+  for (let coin of coins) {
+    const datas = await puppeteerMagic(coin);
+    instance++;
+
+    const worksheet = workbook.addWorksheet(coin);
+
+    worksheet.getCell("A" + instance.toString()).value = "Date";
+    worksheet.getCell("B" + instance.toString()).value = "Open";
+    worksheet.getCell("C" + instance.toString()).value = "High";
+    worksheet.getCell("D" + instance.toString()).value = "Low";
+    worksheet.getCell("E" + instance.toString()).value = "Close";
+    worksheet.getCell("F" + instance.toString()).value = "Adj Close";
+    worksheet.getCell("G" + instance.toString()).value = "Volume";
+
+    //loop each response
+    for (let i = 0; i <= 90; i++) {
+      //   //Date
+      //   //Open
+      //   //High
+      //   //Low
+      //   //Close
+      //   //Adj Close*
+      //   //Volume
+      const data = datas[i].split("\t");
+      const counter = Number(instance) + Number(i);
+
+      worksheet.getCell("A" + counter.toString()).value = "Date";
+      worksheet.getCell("B" + counter.toString()).value = "Open";
+      worksheet.getCell("C" + counter.toString()).value = "High";
+      worksheet.getCell("D" + counter.toString()).value = "Low";
+      worksheet.getCell("E" + counter.toString()).value = "Close";
+      worksheet.getCell("F" + counter.toString()).value = "Adj Close";
+      worksheet.getCell("G" + counter.toString()).value = "Volume";
+
+      //@TODO: write to file
+    }
   }
-
-  await browser.close();
 })();
 
 function return90DaysDate() {
@@ -44,4 +64,28 @@ function return90DaysDate() {
   const date = d.toString().split(" ");
   const date90Days = date[1] + " " + date[2] + ", " + date[3];
   return date90Days.toString();
+}
+
+async function puppeteerMagic(target: string): Promise<String[]> {
+  const browser = await puppeteer.launch({
+    // headless: false,
+    defaultViewport: {
+      width: 1800,
+      height: 1800,
+    },
+  });
+  const page = await browser.newPage();
+  await page.goto(
+    "https://finance.yahoo.com/quote/BTC-USD/history?p=" + target
+  );
+  let closePrice =
+    '//table[@data-test="historical-prices"]//tbody//tr[2]//td[5]//span';
+  console.log("90 Days Date: ", return90DaysDate());
+  const datas = await page.$$eval("table tbody tr", (tds) =>
+    tds.map((td) => {
+      return (td as HTMLElement).innerText;
+    })
+  );
+  await browser.close();
+  return datas;
 }
