476486dfbb91ed7328136901346b6746b1c6a4b7
Lloyd Liongson <user@LloydLions.local>
2022-03-28 21:38:26 +0800

    project completed

--- index.ts
+++ index.ts
@@ -23,22 +23,23 @@ import ExcelJS from "exceljs";
     console.log(coin, " : Exporting data to file IN PROGRESS.");
     const worksheet = workbook.addWorksheet(coin);
     worksheet.columns = [
-      { header: "Date", key: "date" },
-      { header: "Open", key: "open" },
-      { header: "High", key: "high" },
-      { header: "Low", key: "low" },
-      { header: "Close", key: "close" },
-      { header: "AdjClose", key: "adjClose" },
-      { header: "Volume", key: "volume" },
+      { header: "Date", key: "date" }, //A
+      { header: "Open", key: "open" }, //B
+      { header: "High", key: "high" }, //C
+      { header: "Low", key: "low" }, //D
+      { header: "Close", key: "close" }, //E
+      { header: "AdjClose", key: "adjClose" }, //F
+      { header: "Volume", key: "volume" }, //G
+      { header: "Percent of Change", key: "poc" }, //H
     ];
 
     const close90Days = [];
     const open90Days = [];
     const percetageOfChange = [];
+    const rowValues = [];
 
     for (let i = 0; i <= 90; i++) {
       const data = datas[i].split("\t");
-      const rowValues = [];
       for (let y = 0; y <= data.length; y++) {
         rowValues[y] = data[y];
         if (y == 1) {
@@ -47,14 +48,15 @@ import ExcelJS from "exceljs";
         if (y == 4) {
           close90Days.push(parseFloat(data[y].replace(/,/g, "")));
         }
+        console.log(`${coin} : Exported data ${i}`);
       }
       worksheet.addRow(rowValues);
-      console.log(`${coin} : Exported data ${i}`);
     }
 
     //compute mean/average
     const getValueOfMean = (array: number[]) =>
       array.reduce((a, b) => a + b) / array.length;
+    console.log(coin, " : Mean/Average computation COMPLETED.");
 
     //stdev
     let getValueOfStdev = (data: number[]) => {
@@ -66,12 +68,62 @@ import ExcelJS from "exceljs";
           (data.length - 1)
       );
     };
+    console.log(coin, " : STDEV computation COMPLETED.");
 
     //compute percentage of change
+    let cellPosition = Number(1);
     for (let i = 0; i < close90Days.length; i++) {
       //close divided by open
+      cellPosition++;
       percetageOfChange.push(Number(close90Days[i] / open90Days[i]));
+      worksheet.getCell(`H${cellPosition}.tostring()`).value =
+        percetageOfChange[i];
+      console.log(`${coin} : Determine percent of change for data ${i}`);
     }
+
+    //compute for stdev1, 2 and 3
+    worksheet.getCell("J2").value = "STDEV+3";
+    worksheet.getCell("K2").value =
+      getValueOfMean(close90Days) + getValueOfStdev(close90Days) * 3;
+
+    worksheet.getCell("J3").value = "STDEV+2";
+    worksheet.getCell("K3").value =
+      getValueOfMean(close90Days) + getValueOfStdev(close90Days) * 2;
+
+    worksheet.getCell("J4").value = "STDEV+1";
+    worksheet.getCell("K4").value =
+      getValueOfMean(close90Days) + getValueOfStdev(close90Days);
+
+    worksheet.getCell("J5").value = "AVERAGE";
+    worksheet.getCell("K5").value = getValueOfMean(close90Days);
+
+    worksheet.getCell("J6").value = "STDEV-1";
+    worksheet.getCell("K6").value =
+      getValueOfMean(close90Days) - getValueOfStdev(close90Days);
+
+    worksheet.getCell("J7").value = "STDEV-2";
+    worksheet.getCell("K7").value =
+      getValueOfMean(close90Days) - getValueOfStdev(close90Days) * 2;
+
+    worksheet.getCell("J8").value = "STDEV-3";
+    worksheet.getCell("K8").value =
+      getValueOfMean(close90Days) - getValueOfStdev(close90Days) * 3;
+
+    const stdev =
+      (getValueOfStdev(close90Days) / getValueOfMean(close90Days)) * 100;
+    worksheet.getCell("J9").value = "STDEV %";
+    worksheet.getCell("K9").value = parseFloat(stdev.toString()).toFixed(2);
+
+    const stdev2 = stdev * 2;
+    worksheet.getCell("J10").value = "STDEV2 %";
+    worksheet.getCell("K10").value = parseFloat(stdev2.toString()).toFixed(2);
+
+    const roundedStdev2 = parseFloat(stdev2.toString()).toFixed(2);
+    const leverage =
+      200 / parseFloat(roundedStdev2.toString().replace("%", ""));
+
+    worksheet.getCell("J11").value = "MAX LEVERAGE";
+    worksheet.getCell("K11").value = parseFloat(leverage.toString()).toFixed(0);
 
     console.log(coin, " : Exporting data to file COMPLETED.");
   }
